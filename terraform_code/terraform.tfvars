c4d73b4a1b87934de2f0c4be4ce0000237219ef6
Goodness E. Eboh <ebohgoodness.t@gmail.com>
2024-08-13 13:33:50 +0100

    Retouched codes for readability

--- terraform_code/terraform.tfvars
+++ terraform_code/terraform.tfvars
@@ -1,4 +1,1 @@
-name                = "goody-sock-shop"
-s3_bucket_name      = "sock-shop-terraform-state-bucket"
-dynamodb_table_name = "sock-shop-terraform-locks"
-terraform_state_key = "terraform/state"
+name = "goody-sock-shop"
--- terraform_code/variables.tf
+++ terraform_code/variables.tf
@@ -38,21 +38,3 @@ variable "private_subnets" {
   type        = list(string)
   default     = ["10.123.3.0/24", "10.123.4.0/24"]
 }
-
-# S3 bucket name for Terraform state
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state."
-  type        = string
-}
-
-# DynamoDB table name for Terraform state locking
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for Terraform state locking."
-  type        = string
-}
-
-variable "terraform_state_key" {
-  description = "Key path within the S3 bucket to store the Terraform state file"
-  type        = string
-}
-
